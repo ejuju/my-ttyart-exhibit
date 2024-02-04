34ad5418e3d914d1122ec08599182cd6fb29f0af
ejuju <juju.sel.mail@gmail.com>
2024-02-04 18:32:57 +0100

    detect and restart when game of life stuck

--- pkg/gameoflife/game.go
+++ pkg/gameoflife/game.go
@@ -2,59 +2,53 @@ package gameoflife
 
 import (
 	"fmt"
-	"io"
 	"math/rand"
-	"os"
 	"time"
 
 	"github.com/ejuju/my-ttyart-exhibit/pkg/tty"
-	"golang.org/x/term"
 )
 
 func Run() (err error) {
+	// Hide terminal cursor and restore terminal state on exit.
+	tui := tty.NewTUI()
+	tui.HideCursor()
+	tui.EraseEntireScreen()
+	defer tui.ShowCursor()
+	defer tui.ResetTextStyle()
+
+	// Use terminal raw mode.
+	input := make(chan byte)
+	err = tui.GoListenRaw(input)
+	if err != nil {
+		return fmt.Errorf("use terminal raw mode: %w", err)
+	}
+	defer tui.Restore()
+
+	// Create new random grid.
+	width, height, err := tui.Size()
+	if err != nil {
+		return fmt.Errorf("get terminal size: %w", err)
+	}
+	width = width / 2   // We use two-character of text width per cell.
+	height = height - 2 // We use the bottom two lines for banner.
+	g := game{cells: makeCells(width, height-2)}
+
 Base:
+	// Run game loop.
 	for {
-		// Hide terminal cursor and restore terminal state on exit.
-		f := os.Stdout
+		g.numRuns++
+		g.generation = 0
+		g.randomize(time.Now().UnixNano())
+		g.render(tui)
 
-		// Create new random grid.
-		width, height, err := term.GetSize(int(f.Fd()))
-		if err != nil {
-			return fmt.Errorf("get terminal size: %w", err)
-		}
-		width = width / 2 // We use two-character of text width per cell.
-		g := makeGrid(width, height)
-		g.randomize(0)
-
-		// Use terminal raw mode.
-		state, err := term.MakeRaw(int(os.Stdin.Fd()))
-		if err != nil {
-			return fmt.Errorf("use terminal raw mode: %w", err)
-		}
-		defer term.Restore(int(os.Stdin.Fd()), state)
-
-		// Read user input.
-		input := make(chan byte, 1)
-		go func() {
-			for {
-				b := [1]byte{}
-				_, err := io.ReadFull(os.Stdin, b[:])
-				if err != nil {
-					panic(err)
-				}
-				input <- b[0]
-			}
-		}()
-
-		// Run game loop.
-		tui := tty.UI{Writer: f}
-		tui.HideCursor()
-		defer tui.ShowCursor()
-		defer tui.ResetTextStyle()
-		ticker := time.NewTicker(75 * time.Millisecond)
+		ticker := time.NewTicker(100 * time.Millisecond)
+		previousPopulationCounts := [2]int{}
 		for {
 			select {
 			case b := <-input:
+				tui.ResetTextStyle()
+				tui.MoveTo(0, 0)
+				tui.EraseEntireScreen()
 				switch b {
 				default:
 					continue Base
@@ -66,40 +60,52 @@ Base:
 				if !changed {
 					return nil
 				}
+				population := g.population()
+				isStuck := previousPopulationCounts[0] == population && previousPopulationCounts[1] == population
+				if isStuck {
+					continue Base
+				}
+				previousPopulationCounts[0] = previousPopulationCounts[1]
+				previousPopulationCounts[1] = population
+
 				g.render(tui)
 			}
 		}
 	}
 }
 
-type grid [][]bool
-
-func (g grid) width() int  { return len(g) }
-func (g grid) height() int { return len(g[0]) }
-
-func makeGrid(width, height int) (g grid) {
-	g = make([][]bool, width)
-	for x := 0; x < g.width(); x++ {
-		g[x] = make([]bool, height)
-	}
-	return g
+type game struct {
+	numRuns    int
+	generation int
+	cells      [][]bool
 }
 
-func (g grid) randomize(seed int64) {
+func (g game) width() int  { return len(g.cells) }
+func (g game) height() int { return len(g.cells[0]) }
+
+func makeCells(width, height int) (cells [][]bool) {
+	cells = make([][]bool, width)
+	for x := 0; x < width; x++ {
+		cells[x] = make([]bool, height)
+	}
+	return cells
+}
+
+func (g game) randomize(seed int64) {
 	randr := rand.New(rand.NewSource(seed))
 	for x := 0; x < g.width(); x++ {
 		for y := 0; y < g.height(); y++ {
-			g[x][y] = randr.Int()%2 == 0
+			g.cells[x][y] = randr.Int()%2 == 0
 		}
 	}
 }
 
-func (g grid) at(x, y int) bool {
-	width, height := len(g), len(g[0])
-	return g[(width+x)%width][(height+y)%height]
+func (g game) at(x, y int) bool {
+	width, height := g.width(), g.height()
+	return g.cells[(width+x)%width][(height+y)%height]
 }
 
-func (g grid) countNeighbours(x, y int) (count int) {
+func (g game) countNeighbours(x, y int) (count int) {
 	for i := y - 1; i <= y+1; i++ {
 		for j := x - 1; j <= x+1; j++ {
 			if i == y && j == x {
@@ -112,8 +118,19 @@ func (g grid) countNeighbours(x, y int) (count int) {
 	return count
 }
 
-func (g *grid) update() (changed bool) {
-	next := makeGrid(len(*g), len((*g)[0]))
+func (g game) population() (count int) {
+	for x := 0; x < g.width(); x++ {
+		for y := 0; y < g.height(); y++ {
+			if g.at(x, y) {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func (g *game) update() (changed bool) {
+	next := makeCells(g.width(), g.height())
 	for x := 0; x < g.width(); x++ {
 		for y := 0; y < g.height(); y++ {
 			count := g.countNeighbours(x, y)
@@ -124,20 +141,32 @@ func (g *grid) update() (changed bool) {
 			}
 		}
 	}
-	(*g) = next
+	g.generation++
+	g.cells = next
 	return changed
 }
 
-func (g grid) render(tui tty.UI) {
+func (g game) render(tui tty.TUI) {
 	for x := 0; x < g.width(); x++ {
 		for y := 0; y < g.height(); y++ {
 			tui.MoveTo(x*2, y)
-			if alive := g[x][y]; alive {
-				tui.SetBackgroundWhite()
+			if alive := g.at(x, y); alive {
+				tui.SetBackgroundRGB(127, 0, 128+uint8((x+y+g.generation)%256)/2)
 			} else {
-				tui.SetBackgroundBlack()
+				tui.SetBackgroundRGB(0, 0, 0)
 			}
 			tui.Print("  ")
 		}
 	}
+
+	tui.SetBackgroundRGB(0, 0, 0)
+
+	tui.MoveTo(0, g.height()+0)
+	tui.Printf("[#%d] Generation %d | Population %d", g.numRuns, g.generation, g.population())
+
+	tui.MoveTo(0, g.height()+1)
+	tui.Print("Press 'q' to quit, press any other key to restart.")
+
+	tui.MoveTo(0, g.height()+2)
+	tui.Print("Game of Life - John Conway.")
 }
--- pkg/tty/tty.go
+++ pkg/tty/tty.go
@@ -3,24 +3,60 @@ package tty
 import (
 	"fmt"
 	"io"
+	"os"
+
+	"golang.org/x/term"
 )
 
-type UI struct{ io.Writer }
+type TUI struct {
+	beforeMakeRaw *term.State
+	stdin, stdout *os.File
+}
 
-func (ui UI) Print(s string) {
-	_, err := io.WriteString(ui, s)
+func NewTUI() (ui TUI) {
+	return TUI{stdin: os.Stdin, stdout: os.Stdout}
+}
+
+func (ui TUI) Restore() (err error) {
+	return term.Restore(int(ui.stdin.Fd()), ui.beforeMakeRaw)
+}
+
+func (ui TUI) Print(s string) {
+	_, err := io.WriteString(ui.stdout, s)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func (ui UI) Printf(f string, args ...any) {
+func (ui TUI) Printf(f string, args ...any) {
 	ui.Print(fmt.Sprintf(f, args...))
 }
 
-func (ui UI) MoveTo(x, y int)     { ui.Printf("\x1b[%d;%dH", y+1, x+1) }
-func (ui UI) HideCursor()         { ui.Print("\x1b[?25l") }
-func (ui UI) ShowCursor()         { ui.Print("\x1b[?25h") }
-func (ui UI) ResetTextStyle()     { ui.Print("\x1b[0m") }
-func (ui UI) SetBackgroundBlack() { ui.Print("\x1b[40m") }
-func (ui UI) SetBackgroundWhite() { ui.Print("\x1b[47m") }
+func (ui TUI) EraseEntireScreen()             { ui.Print("\x1b[2J") }
+func (ui TUI) MoveTo(x, y int)                { ui.Printf("\x1b[%d;%dH", y+1, x+1) }
+func (ui TUI) HideCursor()                    { ui.Print("\x1b[?25l") }
+func (ui TUI) ShowCursor()                    { ui.Print("\x1b[?25h") }
+func (ui TUI) ResetTextStyle()                { ui.Print("\x1b[0m") }
+func (ui TUI) SetBackgroundRGB(r, g, b uint8) { ui.Printf("\x1b[48;2;%d;%d;%dm", r, g, b) }
+
+func (ui TUI) Size() (width, height int, err error) {
+	return term.GetSize(int(ui.stdout.Fd()))
+}
+
+func (ui *TUI) GoListenRaw(input chan byte) (err error) {
+	ui.beforeMakeRaw, err = term.MakeRaw(int(ui.stdin.Fd()))
+	if err != nil {
+		return err
+	}
+	go func() {
+		for {
+			b := [1]byte{}
+			_, err := io.ReadFull(os.Stdin, b[:])
+			if err != nil {
+				panic(err)
+			}
+			input <- b[0]
+		}
+	}()
+	return nil
+}
